3042abd8b98a38f9944fce8dc79790a451bc24af
ziyw <zywong159@gmail.com>
2023-09-14 07:56:12 -0400

    Added hikariCP example

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,20 +1,24 @@
 package org.example;
 
+import com.zaxxer.hikari.HikariDataSource;
+
 import java.sql.*;
 
 public class Main {
 
     public static void main(String[] args) {
+        bareboneConnection();
+        hikariConnection();
+    }
 
+    static void bareboneConnection() {
+        System.out.println("# Connect to DB directly # ");
         // Local postgres connection url
         // String url = "jdbc:postgresql://localhost/testdb?user=ziyan&password=postgres";
-
         // Docker postgres connection url
         String url = "jdbc:postgresql://0.0.0.0:5432/ziyan?user=ziyan&password=postgres";
 
-        try {
-            Connection conn = DriverManager.getConnection(url);
-
+        try (Connection conn = DriverManager.getConnection(url)) {
             PreparedStatement stmt = conn.prepareStatement("SELECT * FROM note;");
             ResultSet rs = stmt.executeQuery();
             while (rs.next()) {
@@ -24,10 +28,32 @@ public class Main {
                 Timestamp createdAt = rs.getTimestamp("created_at");
                 System.out.println(id + title + content + createdAt.toString());
             }
-
-        } catch (Exception e) {
-            System.out.println("Exception is" + e);
+        } catch (SQLException e) {
+            System.out.println("Get Error: " + e);
+            throw new RuntimeException(e);
         }
-
     }
+
+    static void hikariConnection() {
+        System.out.println("# Connect to DB through connection pool # ");
+        HikariDataSource ds = new HikariDataSource();
+        ds.setJdbcUrl("jdbc:postgresql://0.0.0.0:5432/ziyan");
+        ds.setUsername("ziyan");
+        ds.setPassword("postgres");
+
+        try (Connection conn = ds.getConnection()){
+            PreparedStatement stmt = conn.prepareStatement("SELECT * FROM note;");
+            ResultSet rs = stmt.executeQuery();
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                String title = rs.getString("title");
+                String content = rs.getString("content");
+                Timestamp createdAt = rs.getTimestamp("created_at");
+                System.out.println(id + title + content + createdAt.toString());
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
